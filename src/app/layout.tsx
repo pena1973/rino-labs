f4eb92fdbf38c7863d6dea43fb49fd78bb30da80
pena1973 <penapisemnet@gmail.com>
2025-11-28 10:39:53 +0200

    + хедер поправила

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,6 +61,7 @@ export default function RootLayout({
   return (
     <html lang="en" data-theme="light">
       <head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
